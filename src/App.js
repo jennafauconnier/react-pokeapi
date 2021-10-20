e6ea85bca6a34e72bf16ca5c71ac288817c5dbc4
Jenna <jenna.fauconnier@gmail.com>
2021-10-20 10:29:49 +0200

    add loader gif

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import './App.css';
 import Axios from 'axios';
+import PokeballGif from './pokeball.gif';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonChosen, setPokemonChosen] = useState(false);
   const [pokemon, setPokemon] = useState({
@@ -15,6 +17,7 @@ function App() {
     type: "",
   });
 
+ 
   const searchPokemon = () => {
     Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
       (response) => {
@@ -28,6 +31,7 @@ function App() {
         type: response.data.types[0].type.name,
       });
       setPokemonChosen(true);
+      setIsLoading(false)
     });
   }
 
@@ -36,15 +40,18 @@ function App() {
       <div className="TitleSection">
         <h1>Pokemon Stats</h1>
         <input type="text" onChange={(event) => {setPokemonName(event.target.value)}}/>
-        <button onClick={searchPokemon}>Search Pokemon</button>
+        <button onClick={searchPokemon && setIsLoading}>Search Pokemon</button>
       </div>
       <div className="DisplaySection">
-        {!pokemonChosen ? (
+        {!isLoading && !pokemonChosen ? (
+          <>
           <h1>Please choose a pokemon</h1>
+          <img src={PokeballGif} alt="loading" />
+          </>
         ) : (
           <>
           <h1>{pokemon.name}</h1>
-          <img  src={pokemon.img} />
+          <img src={pokemon.img} alt="" />
           <h3>Species: {pokemon.species}</h3>
           <h3>Type: {pokemon.type}</h3>
           <h4>Hp: {pokemon.hp}</h4>
